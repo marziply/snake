b9fb608cac7ad56e7fffb3d9128f00e8a1e9e721
Hayden Perry <haydencwperry@gmail.com>
2020-11-12 18:36:44 +0000

    Uncommented everything

--- src/events.c
+++ src/events.c
@@ -28,15 +28,15 @@ void exec_event(Events *events, Map *map) {
   switch (events->sdl_event.type) {
   case SDL_QUIT:
     return exit(0);
-    // case SDL_KEYUP:
-    //   exec_key_press(events, NULL);
-    //   break;
-    // case SDL_USEREVENT:
-    //   direction = (int *) events->sdl_event.user.data1;
-    //   tile = map->tiles[map->head_pos.x][map->head_pos.y];
-    //
-    //   tile->direction = (*direction) + 1;
-    //   break;
+  case SDL_KEYUP:
+    exec_key_press(events, NULL);
+    break;
+  case SDL_USEREVENT:
+    direction = (int *) events->sdl_event.user.data1;
+    tile = map->tiles[map->head_pos.x][map->head_pos.y];
+
+    tile->direction = (*direction) + 1;
+    break;
   }
 }
 
@@ -50,40 +50,37 @@ void poll(Map map) {
   Events events;
   Display *display = map.display;
 
-  // map.head_pos.x = rand() % WIN_TILE_WIDTH;
-  // map.head_pos.y = rand() % WIN_TILE_HEIGHT;
-  //
-  // Tile *tile = map.tiles[map.head_pos.x][map.head_pos.y];
-  //
-  // printf("tile: %p\n", (void *) tile);
-  //
-  // tile->is_head = true;
-  // printf("head: %d\n", tile->is_head);
-  // tile->colour = WHITE;
-  // tile->direction = (rand() % 4) + 1;
-  //
-  // events.key_event.type = SDL_RegisterEvents(1);
+  map.head_pos.x = rand() % WIN_TILE_WIDTH;
+  map.head_pos.y = rand() % WIN_TILE_HEIGHT;
+
+  Tile *tile = map.tiles[map.head_pos.x][map.head_pos.y];
+
+  tile->is_head = true;
+  tile->colour = WHITE;
+  tile->direction = (rand() % 4) + 1;
+
+  events.key_event.type = SDL_RegisterEvents(1);
 
   while (true) {
-    // if (time_since >= 1.0) {
-    //   time_since = 0.0;
-    //
-    //   update();
-    // }
+    if (time_since >= 1.0) {
+      time_since = 0.0;
+
+      update();
+    }
 
     if (SDL_PollEvent(&events.sdl_event)) {
       exec_event(&events, &map);
     }
 
-    // time_since += TICK;
+    time_since += TICK;
 
-    // iter_tiles((void *) &paint_tiles, NULL);
+    iter_tiles((void *) &paint_tiles, NULL);
 
-    // SDL_RenderPresent(map.display->renderer);
-    //
-    // SDL_SetRenderDrawColor(display->renderer, BLACK.r, BLACK.g, BLACK.b,
-    // 255); SDL_RenderClear(display->renderer);
-    //
-    // SDL_Delay(TICK);
+    SDL_RenderPresent(map.display->renderer);
+
+    SDL_SetRenderDrawColor(display->renderer, BLACK.r, BLACK.g, BLACK.b, 255);
+    SDL_RenderClear(display->renderer);
+
+    SDL_Delay(TICK);
   }
 }
--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@
 void init() {
   if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     printf("error: %s\n", SDL_GetError());
-    // print_error("Init");
 
     exit(1);
   }
@@ -21,26 +20,19 @@ int main(int argc, char **argv) {
     printf("error: %s", SDL_GetError());
   }
 
-  // init();
+  init();
 
-  // Display display = create_display();
-  // Map map = create_map(display);
-  //
-  // poll(map);
-  // SDL_Window *window = SDL_CreateWindow(
-  //     WIN_TITLE,
-  //     SDL_WINDOWPOS_UNDEFINED,
-  //     SDL_WINDOWPOS_UNDEFINED,
-  //     -1,
-  //     -1,
-  //     SDL_WINDOW_OPENGL
-  // );
+  Display display = create_display();
+  Map map = create_map(display);
 
-  printf("test\n");
+  poll(map);
 
-  // SDL_Event event;
-  //
-  // SDL_PollEvent(&event);
+  SDL_CreateWindow(WIN_TITLE, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                   -1, -1, SDL_WINDOW_OPENGL);
+
+  SDL_Event event;
+
+  SDL_PollEvent(&event);
 
   return 0;
 }
